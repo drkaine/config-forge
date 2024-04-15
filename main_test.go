38812a75f05a3436801c3ca5cc28da1d5ec49998
drkaine <duretkevin@live.fr>
2024-04-15 10:39:31 +0000

    Refactoring tests

--- main_test.go
+++ main_test.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"testing"
 )
 
+var configAccepted []string
+
+func TestMain(m *testing.M) {
+	configAccepted = []string{
+		"1 apache",
+		"2 ngnix",
+	}
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
 func TestChoices(t *testing.T) {
 	mainf := Choices()
-	want := "1 apache 2 ngnix"
+	want := strings.Join(configAccepted, " ")
 
 	if mainf != want {
 		t.Errorf("The choices don't correspond")
 	}
 }
 
-func TestWrongChoice(t *testing.T) {
+func TestWrongChoiceResponded(t *testing.T) {
 	choose := "0"
 	mainf := ChoiceResponded(choose)
-	want := "Wrong choice, choose 1 apache 2 ngnix"
 
-	if mainf != want {
+	if InArray(mainf, configAccepted) {
 		t.Errorf("Good choice return")
 	}
+}
+
+func InArray(search string, target []string) bool {
+	for _, value := range target {
+		if value == search {
+			return true
+		}
+	}
+	return false
 }
