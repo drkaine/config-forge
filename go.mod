874aca896aa259b3e2c2f637f7900ead5422fd71
drkaine <duretkevin@live.fr>
2025-02-10 07:44:41 +0000

    change version

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module config-forge
 
-go 1.21.1
+go 1.22.2
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
 import (
-	"config-forge/configs"
-	"config-forge/runner"
-	"fmt"
-	"os"
+	"config-forge/cmd"
 )
 
 func main() {
-	switch os.Args[1] {
-	case "-c":
-		runner.CLI()
-	case "--cli":
-		runner.CLI()
-	default:
-		runner.Prompt()
-	}
-
-	fmt.Println(configs.GOODBY_RETURN)
+	cmd.Execute()
 }
